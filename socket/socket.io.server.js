0a47d884e0e7c6db5b8b03fd70d2217b80bc5a12
songjein <jeinsong100@gmail.com>
2016-11-18 16:59:39 +0000

    웹 소켓 이벤트 연결 해보기

--- socket/socket.io.server.js
+++ socket/socket.io.server.js
@@ -14,11 +14,17 @@ var server = http.createServer(function(req, res){
 
 
 // 소켓 서버 생성 및 실행
+// 웹 소켓 서버 포트 번호 입력 가능하지만, 일반적으로 웹 서버와 함께 사용하므로..
 var io = socketio.listen(server);
 
 // connection 이벤트 연결 (클라이언트가 웹 소켓 서버에 접속할 때 발생)
 io.sockets.on('connection', function(socket){
-
+	// abc event
+	socket.on('abc', function(data){
+		console.log('Client Send : ' , data);	
+		// 클라이언트에 cba 이벤트 발생시키기
+		socket.emit('abcde', data);
+	});
 });
 
 // 현재는 개발하는 모드이므로 디버그모드 활성화
