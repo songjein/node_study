d77a6e437e3a6be7573756e228f181cffacff360
songjein <jeinsong100@gmail.com>
2016-11-19 16:41:09 +0000

    방 보충 설명

--- socket/room_group.js
+++ socket/room_group.js
@@ -15,6 +15,7 @@ var io = require('socket.io').listen(server);
 
 // 소켓 서버 이벤트 연결
 io.sockets.on('connection', function(socket){
+	// 세션별로 생성되나바?
 	var roomName = null;
 
 	// join event
@@ -29,3 +30,19 @@ io.sockets.on('connection', function(socket){
 		io.sockets.in(roomName).emit('message', 'test');	
 	});
 });
+
+/*
+	소켓 하나는 여러 개의 방에 동시에 존재할 수 있다.
+	따라서 join() 메서드를 여러 번 호출하면 여러개의 방에 들어갈 수 있다.
+	socket.rooms를 출력해보면 현재 어떤 방에 들어가있는지 알 수 있다.
+
+	아! 그리고 to, in 메서드가 같은 의미를 갖는다고해서 의아했는데
+
+	방 하나에 조인했을 때, soket.rooms를 찍어보면
+	두개의 방이 나오는데, 
+	하나는 socket.io가 소켓을 생성할 때 자동으로 자신의 id로 방을 만든 것이다.
+	독방이라 할 수 있다.
+
+	따라서 private 통신을 할 때도  to(), in() 메서드를 쓴것이다.
+
+*/
