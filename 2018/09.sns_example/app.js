74aade3e24797e4f617b66acc764e083cf7cae29
songjein <jeinsong100@gmail.com>
2019-01-01 07:12:01 +0000

    etc

--- 2018/09.sns_example/app.js
+++ 2018/09.sns_example/app.js
@@ -26,7 +26,7 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false} ));
+app.use(express.urlencoded({ extended: false } ));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
 	resave: false,
